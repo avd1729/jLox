57a70e2658f8e87d3a6b89313047f85a889fc711
Aravind <aravind19112003@gmail.com>
2025-05-28 15:14:58 +0530

    define the expression class

---
+++ src/Expr.java
@@ -0,0 +1,14 @@
+abstract class Expr {
+
+    static class Binary extends Expr {
+        final Expr left;
+        final Token operand;
+        final Expr right;
+
+        Binary(Expr left, Token operand, Expr right){
+            this.left = left;
+            this.operand = operand;
+            this.right = right;
+        }
+    }
+}
